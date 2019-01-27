bcd23ca7b9e94f017805014af7a013fbff0bb2e5
James Tyler <macndchase@gmail.com>
2019-01-27 12:59:29 -0500

    adjustments to the css

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ function formatParams(){
 function topHeadlines(){
     $('.openPage').addClass('hidden');
     $('.news').empty();
-    $('.news').append(`<p class="openingStatement1 row">Top Headlines!</p><p class="openingStatement2">Here you can find the top headlines for a given topic, or simply top headlines for by the country and catagory! Try for yourself to see what is breaking the news.</p><form class="allTheOptions"><button type="submit" class="beginFetch" role="button">submit</button>
-    <div class="left"><select name="allOrTop" id="option1or2" class="hidden"><option value="top-headlines?" >Top Headlines?</option></select><br><label for="searchQ"> Topic?</label><input class="searchQ" name="searchQ" id="searchQ" type="text" placeholder="Topic of interest" contenteditable="true" aria-placeholder="Topic of interest" ><br><label for="cC"> Country code</label><input class="countrySearch" name="countryCode" id="cC" type="text" placeholder="US,DE,JP,FR,IT" contenteditable="true" aria-placeholder="US,DE,JP,FR,IT" ><br><label for="catagories" class="question"> Catagories</label><select name="catsearch" id="catagories"><option value="none" checked>None</option><option value="entertainment">Entertainment</option><option value="business">Business</option><option value="general">General</option><option value="health">Health</option><option value="science">Science</option><option value="technology">Technology</option><option value="sports">Sports</option></select><br><label for="numResult"> Ideal article count</label><input class="maxResults" type="number" value="10" id="numResult" name="numResults" contenteditable="true"  ></div></form><p class="err"></p><section class="hidden " id="resultsSection"><h2 class="nR"> News Results!</h2><ul class="printResults "></ul></section>`);
+    $('.news').append(`<p class="openingStatement1 row">Top Headlines!</p><p class="openingStatement2">Here you can find the top headlines for a given topic, or simply top headlines for by the country and catagory! Try for yourself to see what is breaking the news.(Be wary you cant use symbols)</p><form class="allTheOptions"><button type="submit" class="beginFetch" role="button">submit</button>
+    <div class="left"><select name="allOrTop" id="option1or2" class="hidden"><option value="top-headlines?" >Top Headlines?</option></select><br><label for="searchQ"> Topic?</label><br><input class="searchQ" name="searchQ" id="searchQ" type="text" placeholder="Topic of interest" contenteditable="true" aria-placeholder="Topic of interest" ><br><label for="cC"> Country code</label><br><input class="countrySearch" name="countryCode" id="cC" type="text" placeholder="US,DE,JP,FR,IT" contenteditable="true" aria-placeholder="US,DE,JP,FR,IT" ><br><label for="catagories" class="question"> Catagories</label><br><select name="catsearch" id="catagories"><option value="none" checked>None</option><option value="entertainment">Entertainment</option><option value="business">Business</option><option value="general">General</option><option value="health">Health</option><option value="science">Science</option><option value="technology">Technology</option><option value="sports">Sports</option></select><br><label for="numResult"> Ideal article count</label><br><input class="maxResults" type="number" value="10" id="numResult" name="numResults" contenteditable="true"  ></div></form><p class="err"></p><section class="hidden " id="resultsSection"><h2 class="nR"> News Results!</h2><ul class="printResults "></ul></section>`);
 }
 
 function everything(){
     $('.openPage').addClass('hidden');
     $('.news').empty();
-    $('.news').append(`<p class="openingStatement1 row">Everything!</p><p class="openingStatement2">Here you can find the everything for a given topic(be mindful that everything is broad so topics are a must)! Try for yourself to see what is breaking the news.</p><form class="allTheOptions"><button type="submit" class="beginFetch" role="button">submit</button><div class="left"><select name="allOrTop" id="option1or2" class="hidden" required><option value="everything?">Everything?</option></select><br><label for="searchQ"> Topic?</label><input class="searchQ" name="searchQ" type="text" id="searchQ" placeholder="Topic of interest" contenteditable="true" aria-placeholder="Topic of interest"  required><br><label for="catagories" class="question"> Catagories</label><select name="catsearch" id="catagories"><option  value="none" checked>None</option><option value="entertainment">Entertainment</option><option value="business">Business</option><option value="general">General</option><option value="health">Health</option><option value="science">Science</option><option value="technology">Technology</option><option value="sports">Sports</option></select><br><label for="numResult"> Ideal article count</label><input class="maxResults" type="number" value="10" id="numResult" name="numResults" contenteditable="true"  ></div></form><p class="err"></p><section class="hidden " id="resultsSection"><h2 class="nR row"> News Results!</h2><ul role="list" class="printResults "></ul></section>`);
+    $('.news').append(`<p class="openingStatement1 row">Everything!</p><p class="openingStatement2">Here you can find the everything for a given topic(be mindful that everything is broad so topics are a must)! Try for yourself to see what is breaking the news.(Be wary you cant use symbols)</p><form class="allTheOptions"><button type="submit" class="beginFetch" role="button">submit</button><div class="left"><select name="allOrTop" id="option1or2" class="hidden" required><option value="everything?">Everything?</option></select><br><label for="searchQ"> Topic?</label><br><input class="searchQ" name="searchQ" type="text" id="searchQ" placeholder="Topic of interest" contenteditable="true" aria-placeholder="Topic of interest"  required><br><label for="catagories" class="question"> Catagories</label><br><select name="catsearch" id="catagories"><option  value="none" checked>None</option><option value="entertainment">Entertainment</option><option value="business">Business</option><option value="general">General</option><option value="health">Health</option><option value="science">Science</option><option value="technology">Technology</option><option value="sports">Sports</option></select><br><label for="numResult"> Ideal article count</label><br><input class="maxResults" type="number" value="10" id="numResult" name="numResults" contenteditable="true"  ></div></form><p class="err"></p><section class="hidden " id="resultsSection"><h2 class="nR row"> News Results!</h2><ul role="list" class="printResults "></ul></section>`);
     console.log('testing 123');
 }
 
@@ -60,18 +60,18 @@ $('.printResults').append(`<li role="listitem" class="articleCount row">Total av
       `<li  role="listitem" class="hmm"><div class="article"><div class="articleHead row"><h2><a href="${responseJson.articles[i].url}">${responseJson.articles[i].title}</a></h3>
       <p>${responseJson.articles[i].author}</p><p role="link"><a  href="${responseJson.articles[i].url}">
       Link to Article</a></p>
-      <p class="dateColor">${dateStr}</p></div><br>
-      <div class="articleBody ">
-      <img src='${responseJson.articles[i].urlToImage}' class="pParker row" alt="photo from article of news"/><br>
-      <p>${responseJson.articles[i].description}</p>
-      </div></div>
+      <p class="dateColor">${dateStr}</p></div><div class="toTheRight">
+      <p >${responseJson.articles[i].description}</p>
+      </div>
+      <img src='${responseJson.articles[i].urlToImage}' class="pParker" alt="photo from article of news"/>
       </li>`
     );}
   
   $('#resultsSection').removeClass('hidden');
 }
 function testArticle(responseJson){
-
+console.log("article");
+console.log(responseJson);
     for (let i = 0; i < responseJson.articles.length; i++){
 
     let timeStr = (responseJson.articles[i].publishedAt);
@@ -85,10 +85,9 @@ function testArticle(responseJson){
       `<li role="listitem" class="hmm"><div class="articleHead row"><h2><a href="${responseJson.articles[i].url}">${responseJson.articles[i].title}</a></h3>
       <p>${responseJson.articles[i].author}</p><p role="link"><a  class="dateColor" href="${responseJson.articles[i].url}">
       Link to Article </a></p>
-      <p class="dateColor">${dateStr}</p></div><br>
-      <div class="articleBody ">
-      <img src='${responseJson.articles[i].urlToImage}' class="pParker row" alt="photo from article of news"/><br>
-      <p>${responseJson.articles[i].description}</p>
+      <p class="dateColor">${dateStr}</p></div>
+      <img src='${responseJson.articles[i].urlToImage}' class="pParker" alt="photo from article of news"/><div class="toTheRight">
+      <p >${responseJson.articles[i].description}</p>
       </div>
       </li>`
     );}
@@ -105,7 +104,8 @@ function loadingPage(){
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => testArticle(responseJson))
+    .then(responseJson => testArticle(responseJson));
+    
 }
 
 function getTheNews(params){
